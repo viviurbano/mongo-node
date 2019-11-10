f8a2d78859ef49a72b332394f46a0f6a0ee72712
viviurbano <viviane.urbano1@gmail.com>
2019-11-10 19:58:58 -0300

    Campos paciente

--- Projeto_Mongo_Node/routes/index.js
+++ Projeto_Mongo_Node/routes/index.js
@@ -6,42 +6,50 @@ var url = 'mongodb://localhost:27017/';
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  mongoClient.connect(url, function(err, db){
+  mongoClient.connect(url, function (err, db) {
     if (err) throw err;
-      console.log("Conectado");  
-    var dbo = db.db("internacao");
-    dbo.collection('internacao').find({}).toArray(function(er, result){
+    console.log("Conectado");
+    var dbo = db.db("controle_internacao");
+    dbo.collection('controle_internacao').find({}).toArray(function (er, result) {
       if (er) throw er;
       //console.log(result);
       res.render('index', { title: 'Pacientes', nomes: result });
       db.close();
     });
-  }); 
+  });
 });
 
-router.post('/request', function(req, res, next) {
-  console.log('AQUI')
+router.post('/request', function (req, res, next) {
 
   var request_method = req.body._request_method
   var elem_id = req.body._element_id
   var cod = req.body.cod
   var nome = req.body.nome
-  var idade = req.body.idade
+  var endereco = req.body.endereco
+  var dt_nascto = req.body.dt_nascto
+  var telefone = req.body.telefone
+  var sexo = req.body.sexo
+  var tipo_sanguineo = req.body.tipo_sanguineo
 
   obj = {
-    "cod" : cod,
-    "nome" : nome,
-    "idade" : idade
+    "cod": cod,
+    "nome": nome,
+    "endereco": endereco,
+    "dt_nascto": dt_nascto,
+    "telefone": telefone,
+    "sexo": sexo,
+    "tipo_sanguineo": tipo_sanguineo
+
   }
 
-  if(request_method === 'delete'){
+  if (request_method === 'delete') {
 
-    mongoClient.connect(url, function(err, db){
+    mongoClient.connect(url, function (err, db) {
       if (err) throw err;
-      var dbo = db.db("internacao");
-      dbo.collection('internacao').deleteOne({ cod: elem_id}, function(er, result){
+      var dbo = db.db("controle_internacao");
+      dbo.collection('controle_internacao').deleteOne({ cod: elem_id }, function (er, result) {
         if (er) throw er;
         res.redirect('/')
         db.close();
@@ -49,43 +57,43 @@ router.post('/request', function(req, res, next) {
     });
 
   }
-  else{
+  else {
 
-    mongoClient.connect(url, function(err, db){
+    mongoClient.connect(url, function (err, db) {
       if (err) throw err;
-      var dbo = db.db("internacao");
+      var dbo = db.db("controle_internacao");
 
-      console.log('FABIOOOOOOOO'+obj.cod)
-
-      dbo.collection('internacao').findOne({ cod: obj.cod}, function(er, result){
+      dbo.collection('controle_internacao').findOne({ cod: obj.cod }, function (er, result) {
         if (er) throw er;
 
-        // something found -> update
-console.log('FABIOOOOOOOO::'+result)
+        if (result) {
 
-        if(result){
-
-            mongoClient.connect(url, function(err, db){
-              if (err) throw err;
-              var dbo = db.db("internacao");
-              dbo.collection('internacao').updateOne(
-                {"_id" : result._id },
-                { $set: {"nome": obj.nome} } 
-              , function(er, result){
+          mongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var dbo = db.db("controle_internacao");
+            dbo.collection('controle_internacao').updateOne(
+              { "_id": result._id },
+              { $set: { "nome": obj.nome, 
+                        "endereco": obj.endereco,
+                        "dt_nascto": obj.dt_nascto,
+                        "telefone": obj.telefone,
+                        "sexo": obj.sexo,
+                        "tipo_sanguineo": obj.tipo_sanguineo  } }
+              , function (er, result) {
                 if (er) throw er;
                 res.redirect('/')
                 db.close();
               });
-            });
+          });
 
         }
         // nothing found -> insert
-        else{
+        else {
 
-          mongoClient.connect(url, function(err, db){
+          mongoClient.connect(url, function (err, db) {
             if (err) throw err;
-            var dbo = db.db("internacao");
-            dbo.collection('internacao').insertOne(obj, function(er, result){
+            var dbo = db.db("controle_internacao");
+            dbo.collection('controle_internacao').insertOne(obj, function (er, result) {
               if (er) throw er;
               res.redirect('/')
               db.close();
@@ -98,7 +106,7 @@ console.log('FABIOOOOOOOO::'+result)
     });
 
   }
-  
+
 });
 
 module.exports = router;
